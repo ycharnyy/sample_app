445f67b4066adf402002513b3dd7c971d17bd459
Yan <ycharnyy@gmail.com>
2015-01-25 16:24:22 +0300

    Finish layout and routes

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+def full_title(page_title)
+  base_title = "Ruby on Rails Tutorial Sample App"
+  if page_title.empty?
+    base_title
+  else
+    "#{base_title} | #{page_title}"
+  end
+end
+
+describe "User pages" do
+
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
+end
